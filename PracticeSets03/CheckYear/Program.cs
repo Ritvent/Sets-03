b25ddf0b9447b86a5c1652f86a48683066c3c0c2
Ritvent <ivanventurilloq@gmail.com>
2025-01-06 06:54:30 +0800

    init commit

---
+++ PracticeSets03/CheckYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace CheckYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check year if it's a leap year");
+            Console.Write("Enter year: ");
+
+            int year = int.Parse(Console.ReadLine());
+
+            if (CheckLeapYear(year))
+            {
+                Console.WriteLine($"The year {year} is a leap year. ");
+            }
+            else
+            {
+                Console.WriteLine($"The year {year} is not a leapy year. ");
+            }
+        }
+        static bool CheckLeapYear(int year)
+        {
+            if ((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PracticeSets03/Heron's Formula/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace FormulaHeron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first side: ");
+            double sideA = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second side: ");
+            double sideB = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter third side: ");
+            double sideC = int.Parse(Console.ReadLine());
+
+
+            double area = HeronFormula(sideA, sideB, sideC);
+            Console.WriteLine($"The area using Heron's formula equals to {area}");
+
+
+        }
+        static double HeronFormula(double sideA, double sideB, double sideC)
+        {
+            double s = (sideA + sideB + sideC) / 2;
+            double area = Math.Sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+
+            return area;
+        }
+    }
+}
